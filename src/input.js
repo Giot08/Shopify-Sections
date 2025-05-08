098906649de996c354afbb08b7d61febdc601752
Giot08 <xgiot08@gmail.com>
2025-05-08 01:15:44 -0400

    Actualizar package.json y package-lock.json para incluir Tailwind CSS y esbuild como dependencias. Se modifican scripts para la construcción y sincronización del tema, y se corrige la sintaxis en theme-sync.js. Además, se ajusta la estructura de giot-product-cards.liquid y se actualiza index.json para reflejar cambios en la configuración de secciones.

---
+++ src/input.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded');
+    console.log('giot is the best');
+});
+
--- src/theme-sync.js
+++ src/theme-sync.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 try {
   process.chdir(path.join(__dirname, '../theme'));
-  const command = `theme get --password=${process.env.SHOPIFY_PASSWORD} --store='${process.env.SHOPIFY_STORE}' --themeid=${process.env.SHOPIFY_THEME_ID}`;
+  const command = `theme get --password=${process.env.SHOPIFY_PASSWORD} --store="${process.env.SHOPIFY_STORE}" --themeid=${process.env.SHOPIFY_THEME_ID}`;
     execSync(command, { stdio: 'inherit' });
   
   console.log('✅ Sincronización completada exitosamente');
